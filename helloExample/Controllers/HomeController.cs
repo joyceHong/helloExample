813d360d840b41f3f7f47f0f2217665efe0da2e4
joyceHong <joyceHong0827@gmail.com>
2016-09-13 10:42:31 +0800

    test

--- helloExample/Controllers/HomeController.cs
+++ helloExample/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace helloExample.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your test";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your test";
 
             return View();
         }
